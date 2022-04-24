665b6991485151c52b81278ea73b4656f46277a4
Romain Crestey <rcrestey@gmail.com>
2022-04-24 19:29:25 +0200

    use createRoot

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 
 import './reset.css';
 import './index.css';
 
 const pokemon = <div>Pikachu</div>;
 
-ReactDOM.render(pokemon, document.getElementById('root'));
+// ReactDOM.render(pokemon, document.getElementById('root'));
+const target = document.getElementById('root') as Element;
+const root = ReactDOM.createRoot(target);
+
+root.render(pokemon);
