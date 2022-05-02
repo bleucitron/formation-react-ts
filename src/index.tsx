74262fab8b147f8a4b63bfcc2fbc2e710bc0672a
Romain Crestey <rcrestey@gmail.com>
2022-05-02 09:00:27 +0200

    fix: re use old root render for simplicity

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 
 import './reset.css';
 import './index.css';
 
 const pokemon = <div>Pikachu</div>;
 
-// ReactDOM.render(pokemon, document.getElementById('root'));
-const target = document.getElementById('root') as Element;
-const root = ReactDOM.createRoot(target);
+ReactDOM.render(pokemon, document.getElementById('root'));
 
-root.render(pokemon);
+/**
+ * // React 18:
+ * const target = document.getElementById('root') as Element;
+ * const root = ReactDOM.createRoot(target);
+ * root.render(pokemon);
+ */
